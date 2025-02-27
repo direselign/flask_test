386efee812f0556ee4d83131b93a733eb477a4c1
Direselign Addis <direselign@gmail.com>
2025-02-27 18:49:59 +0300

    fix model

--- models.py
+++ models.py
@@ -6,9 +6,8 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    password = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=True)
 
     def __repr__(self):
         return f'<User {self.username}>' 
